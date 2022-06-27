b800749c4ad6f10e8d035526635b30697bcc7fc7
Steven Kwok <derekkwok2020@gmail.com>
2022-06-27 09:22:03 +0100

    Test Creating
    Creating several method for testing

---
+++ MonstrousDaoTestingLab/src/test/java/com/sg/testing/dao/MonsterDaoTest.java
@@ -0,0 +1,26 @@
+package com.sg.testing.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonsterDaoTest {
+    public void testAddMonster() {
+
+    }
+
+    public void testGetMonster() {
+
+    }
+
+    public void testGetAllMonsters () {
+
+    }
+
+    public void testUpdateMonster() {
+
+    }
+
+    public void testRemoveMonster() {
+
+    }
+
+}
